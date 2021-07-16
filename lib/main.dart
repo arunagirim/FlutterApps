cd295507c3a329ba348fee27de23a0de26dac4df
arunagirim <arunagiri350@gmail.com>
2021-07-16 09:57:35 +0530

    Project Upload

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:notesapp/pages/login.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Notes',
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.purple,
+        accentColor: Colors.purple,
+        scaffoldBackgroundColor: Color(0xff070706),
+      ),
+      home: LoginPage(),
+    );
+  }
+}
